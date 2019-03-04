55b31de2f7f1a3b0708fd2bf47fabed7628bbc90
johnfn <johnfn@gmail.com>
2019-03-03 16:26:55 -0800

    Choppy movement

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -1,5 +1,7 @@
-
 export class C {
   public static GAME_WIDTH  = 800;
   public static GAME_HEIGHT = 600;
+
+  public static TILE_WIDTH  = 32;
+  public static TILE_HEIGHT = 32;
 }
--- src/game/player.ts
+++ src/game/player.ts
@@ -1,15 +1,22 @@
 import { Entity } from "./entity";
 import { GameState } from "./state";
 import { Graphics } from "pixi.js";
+import { C } from "./constants";
 
 export class Player extends Entity {
+  mapX = 0;
+  mapY = 0;
+
   constructor(state: GameState) {
     super({
       state,
       parent: state.app.stage,
-      x     : 100,
-      y     : 100,
+      x     : 32 * 5,
+      y     : 32 * 5,
     });
+
+    this.mapX = 5;
+    this.mapY = 5;
 
     const graphics = new Graphics();
     
@@ -20,17 +27,20 @@ export class Player extends Entity {
   }
 
   update() {
-    if (this.state.keyboard.down.A) {
-      this.x -= 5;
+    if (this.state.keyboard.justDown.A) {
+      this.mapX -= 1;
     }
-    if (this.state.keyboard.down.D) {
-      this.x += 5;
+    if (this.state.keyboard.justDown.D) {
+      this.mapX += 1;
     }
-    if (this.state.keyboard.down.W) {
-      this.y -= 5;
+    if (this.state.keyboard.justDown.W) {
+      this.mapY -= 1;
     }
-    if (this.state.keyboard.down.S) {
-      this.y += 5;
+    if (this.state.keyboard.justDown.S) {
+      this.mapY += 1;
     }
+
+    this.x = this.mapX * C.TILE_WIDTH;
+    this.y = this.mapY * C.TILE_HEIGHT;
   }
 }
